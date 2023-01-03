5c19c2a9ab4e42a91b0f86f6a8af9389c1accddc
Peter Chen <peterchen2017125@gmail.com>
2023-01-03 20:23:53 +0800

    20230103

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,93 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('javaScript is Fun!');
+
+// 40 - 8 + 23 - 10;
+// console.log(40 - 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let Person = 'jonas'
+// let PI = 3.14159;
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+
+// let age = 30;
+// age = 31;
+
+// console.log(age);
+
+// const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+// var  job = 'pogrammer';
+// job = 'teacher'
+
+// LastName = 'Schmedtmann';
+// console.log(LastName);
+
+
+
+
+
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
