07007827b003842afe5b467ab0efacd86bc0e4f0
Peter Chen <peterchen2017125@gmail.com>
2023-01-04 21:03:49 +0800

    20230104

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -124,19 +124,28 @@
 
 // console.log(BMIMark, BMIJohn, markHigherBMI);
 
-const firstName = 'Jonas';
-const job = 'teacher';
-const birthYear = 1991;
-const year = 2037;
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
 
-const jonas = "I'm" + firstName + ', a' + (year - birthYear) + 'years old' + job + '!';
-const jonasNew = `I'm ${firstName} a ${year - birthYear} years old ${job} !`;
-console.log(jonasNew);
+// const jonas = "I'm" + firstName + ', a' + (year - birthYear) + 'years old' + job + '!';
+// const jonasNew = `I'm ${firstName} a ${year - birthYear} years old ${job} !`;
+// console.log(jonasNew);
 
-console.log(`Just a regular string ...`);
-console.log('String with \n\
-    multiple \n\
-    lines');
-console.log(`String with
-multiple 
-lines`);
+// console.log(`Just a regular string ...`);
+// console.log('String with \n\
+//     multiple \n\
+//     lines');
+// console.log(`String with
+// multiple 
+// lines`);
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years:)`);
+}
