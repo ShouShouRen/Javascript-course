5f6627c279fafe34baecaba0f8edd0c041d249a7
Peter Chen <peterchen2017125@gmail.com>
2023-01-04 20:41:02 +0800

    20230104

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -66,28 +66,53 @@
 
 
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-console.log(firstName + ' ' + lastName);
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
 
-let x = 10 + 5;
-x += 10;
-x *= 4;
-x++;
-x--;
-x--;
-console.log(x);
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
 
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
+
+// const now = 2037
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x,y);
+
+// const averageAge = ageJonas + ageSarah / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+let markHigherBMI = BMIMark > BMIJohn;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+console.log(BMIMark, BMIJohn);
