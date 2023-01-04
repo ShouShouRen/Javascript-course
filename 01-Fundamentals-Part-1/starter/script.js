aef9e84b06b7913642c2b06cfe595efecbb2099f
Peter Chen <peterchen2017125@gmail.com>
2023-01-04 22:00:43 +0800

    20230104

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -182,20 +182,39 @@
 //     console.log(`John's BMI (${BMIJohn}) is higher than Mark's! (${BMIMark})`)
 // }
 
-const inputYear = '1991';
+// const inputYear = '1991';
 
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
 
-console.log(Number('Jonas'));
-console.log(typeof NaN);
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
-console.log('I am ' + 23 + ' years old');
-console.log('23' - '10' - 3);
-console.log('23' * '2');
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' * '2');
 
-let n = '1' + 1;
-n = n - 1;
-console.log(n );
+// let n = '1' + 1;
+// n = n - 1;
+// console.log(n );
+
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 0;
+// if (money) {
+//     console.log("Don't spend it all");
+// } else {
+//     console.log("You should get a job!");
+// }
+
+const age = 18;
+if(age === 18)console.log("You should became an adult:D")
